9bf92939cc340e1df760f9a49888ad10837dc2ad
moonthegreat969 <100451541+moonthegreat969@users.noreply.github.com>
2022-07-23 06:00:52 +0400

    Add files via upload

---
+++ skillings2.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    var x = document.getElementById("hidden") ;
+    if (x.style.visibility === 'hidden'){
+        x.style.visibility = "visible";
+    }
+    else {
+    x.style.visibility = "hidden"
+    }
+}
+
+    function myFunction2() {
+    var x = document.getElementById("hidden2") ;
+    ;
+    if (x.style.display === 'none') {
+        x.style.display = "flex";
+    }
+    else {
+    x.style.display= "none"
+    }
+    
+    }
+
+    //if (y.style.display === "none"){
+    //      x.style.display = "block";
+    //  }
+
